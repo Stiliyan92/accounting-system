df491b1abe635df45eabb87f056378e784905f90
root <root@accounting.ipp.acad.bg>
2016-08-27 11:05:54 +0300

    Added more metainfo

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
-
-setup(name='accounting-system',
-            version='0.1',
-            description='todo',
-            url='https://github.com/Stiliyan92/accounting-system',
-            author='Stiliyan Stoyanov',
-            author_email='sstoyanov@parallel.bas.bg',
-            license='todo',
-            packages=['accounting-system'],
-            zip_safe=False)
-
 from setuptools import setup, find_packages
 
 setup(
     name="accounting-system",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     install_requires=['configparser >= 3.5.0', 'mysqlclient >= 1.3.7',
-                      'pika >= 0.10.0', 'PyMySQL >= 0.7.2'],
+                      'pika >= 0.10.0', 'PyMySQL >= 0.7.2',
+                      'mysqlclient >= 1.3.7'],
     author="Stiliyan Stoyanov",
     author_email="sstoyanov@parallel.bas.bg",
     description="",
-    license="PSF",
+        classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Scientigic users',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     keywords="accounting system hpc grid",
     url="https://github.com/Stiliyan92"
+    package_data={
+        'accouting-system': ['pbs_log', 'tests/example.conf',
+         tests/pbs_test_log],
+        },
 )
