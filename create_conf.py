e439f00037ba913ddd76f169cc5a2950a0a679a9
Stiliyan92 <stiliyan.valeriev@gmail.com>
2016-08-27 11:14:18 +0300

    Removed hardcoded constants

--- create_conf.py
+++ create_conf.py
@@ -2,21 +2,18 @@ import configparser
 import settings as s
 
 Config = configparser.ConfigParser()
-# lets create that config file for next time...
 cfgfile = open(s.PROJECT_DIR + "accounting.conf",'w')
 
-# add the settings to the structure of the file, and lets write it out...
-Config.add_section('HPC CENTRE INFORMATION')
-Config.set('HPC CENTRE INFORMATION','Name','Avitohol')
-Config.set('HPC CENTRE INFORMATION','Country', 'Bulgaria')
+Config.add_section(s.HPC_INFO)
+Config.set(s.HPC_INFO,'Name','Avitohol')
+Config.set(s.HPC_INFO,'Country', 'Bulgaria')
 
+Config.add_section(s.JOB_INFO)
+Config.set(s.JOBINFO,'Queues','')
+Config.set(JOB_INFO,'Groups', 'viseem')
 
-Config.add_section('JOB INFO')
-Config.set('JOB INFO','Queues','')
-Config.set('JOB INFO','Groups', 'viseem')
-
-Config.add_section('PROGRAM SETTINGS')
-Config.set('PROGRAM SETTINGS','Logs_Dir',
+Config.add_section(s.SETTINGS_INFO)
+Config.set(s.SETTINGS_INFO,'Logs_Dir',
            '/home/stiliyan/python/')
 
 Config.write(cfgfile)
